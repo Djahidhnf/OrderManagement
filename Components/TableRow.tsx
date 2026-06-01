d21f251d3e8eb4c14a231f76f4c28a30ba2d6d18
Djahidhnf <hnfdjahid@gmail.com>
2026-06-01 22:47:24 +0200

    feat: show order type under ID and gray out future ship date rows

--- Components/TableRow.tsx
+++ Components/TableRow.tsx
@@ -16,17 +16,22 @@ export default function TableRow({orders, filter, setOrders}: {orders: any[], fi
       router.push(`/order/${id}`);
     }
 
+  const today = new Date().toISOString().split('T')[0];
+
   return (
     <>
-      
       {(filter ? orders.filter(o => o.status === filter) : orders)
       .map((order) => (
-        <tr key={order.id} className="hover:bg-foreground cursor-pointer" 
+        <tr key={order.id}
+        className={`hover:bg-foreground cursor-pointer ${order.ship_date && order.ship_date > today ? 'text-gray-500' : ''}`}
         onDoubleClick={() => handleDoubleClick(order.id)}>
             <td className="border border-gray-600 px-5 w-1/15 py-1">
                 <MoreButton order={order} setOrders={setOrders}/>
             </td>
-            <td className="border border-gray-600 px-5 w-1/16 py-1">{order.id}</td>
+            <td className="border border-gray-600 px-5 w-1/16 py-1">
+                {order.id}
+                <div className="text-xs text-gray-400">{order.order_kind}</div>
+            </td>
             <td className="border border-gray-600 px-5 w-1/16">{order.seller_name}</td>
             <td className="border border-gray-600 px-5 w-2/16 py-1">
                 {order.client_name}<br/>
@@ -37,12 +42,15 @@ export default function TableRow({orders, filter, setOrders}: {orders: any[], fi
             <td className="border border-gray-600 px-5 w-3/16 py-1 text-xs" style={{whiteSpace: 'pre-wrap'}}>{order.notes}</td>
             <td className="border border-gray-600 px-5 w-2/16 py-1">
                 {order.delivery_name}<br/>
-                {order.delivery_phone}<br/>
+                {order.delivery_name === "wordexpress"? "-" : order.delivery_phone}<br/>
                 {order.delivery_name && order.fee + "DA"}</td>
             <td className="border border-gray-600 px-5 w-1/16 py-1">
                 {order.formatted_date}
             </td>
-            <td className="border border-gray-600 px-5 w-1/16 py-1">{order.total}</td>
+            <td className="border border-gray-600 px-5 w-1/16 py-1">
+              {order.fee > 0 && <span className="text-xs text-gray-500">({order.total - order.fee} DA)</span>}<br/>
+              {order.fee > 0 && <span>{order.total} DA</span>}
+            </td>
             <td className="border border-gray-600 px-5 w-1/16 py-1">
                 <StateButton state={order.status} id={order.id} setOrders={setOrders} />
             </td>
